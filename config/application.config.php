33f8633df69096ac3c1dba39add5c623ed456061
Will Salazar <wsalazar@focuscamera.com>
2014-07-11 19:02:46 -0300

    adding and commit app config file for auth module

--- config/application.config.php
+++ config/application.config.php
@@ -15,6 +15,7 @@ return array(
 //        'ZFTool',
         'Search',
         'Users',
+
     ),
     'module_listener_options' => array(
         'config_glob_paths' => array('config/autoload/{,*.}{global,local}.php'),
