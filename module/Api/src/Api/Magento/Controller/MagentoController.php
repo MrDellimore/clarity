da6f163a308f29ef0c4bc75cfba325f497a2c02e
Will Salazar <wsalazar@focuscamera.com>
2014-07-19 11:22:57 -0300

    added Magento to Api module and added a view.

--- module/Api/src/Api/Magento/Controller/MagentoController.php
+++ module/Api/src/Api/Magento/Controller/MagentoController.php
@@ -21,7 +21,62 @@ class MagentoController  extends AbstractActionController {
             $sm = $this->getServiceLocator();
             $this->magentoTable = $sm->get('Api\Magento\Model\MagentoTable');
         }
-        $this->magentoTable->soapContent();
-        return new ViewModel(array('test' => 'Hello World'));
+
+        $skuData = $this->magentoTable->grabSkus();
+//        $dirtyFields = 0;
+//        if('1' == $skuData['state']) {
+//
+//        }
+//        echo "<pre>";
+//        var_dump($skuData);
+        return new ViewModel(array('sku'=>$skuData));
+//        die();
+//        $this->magentoTable->soapContent();
+//        return new ViewModel(array(
+//            'id'    => number_format((float)$skuData['id']),
+//            'sku'    => number_format((float)$skuData['sku']),
+//            'lastModedifiedDate'    => number_format((float)$skuData['lastModedifiedDate']),
+//            'dirtyFields' => number_format((float)$skuData['dirty']),
+//            'cleanFields'   =>  number_format((float)$skuData['clean']),
+//            'newFields' => number_format((float)$skuData['new']),
+//        ));
     }
-} 
+}
+/*
+<tr>
+                                <?php if($index == 'id') {
+    foreach($value as $state => $entityID){
+        if('dirty' == $state ) {
+            foreach($entityID as $id) {
+                ?>
+
+                <td><?=$id?></td>
+            <?php }
+        }
+
+    }
+    ?>
+
+<?php                                      }
+                                      if('sku' == $index) {
+                                          foreach($value as $state => $sku) {
+                                              if('dirty' == $state ) {
+                                                  foreach($sku as $skuValue) {
+                                                      ?>
+                                                      <td><?=$skuValue?></td>
+                                                  <?php
+                                                  }
+                                              }
+                                          }
+                                      }
+                                      if('lastModedifiedDate' == $index){
+                                          foreach($value as $state => $lastDateModified){
+                                              if('dirty' == $state) {
+                                                  foreach($lastDateModified as $date)
+                                                      ?>
+
+                                                      <td><?=$date?></td>
+                                              <?php
+                                              }
+                                          }
+                          */
--- module/Api/src/Api/Magento/Model/MagentoTable.php
+++ module/Api/src/Api/Magento/Model/MagentoTable.php
@@ -9,7 +9,10 @@
 namespace Api\Magento\Model;
 
 use Zend\Db\Adapter\Adapter;
-use Zend\Soap\AutoDiscover;
+use SoapClient;
+use Zend\Db\Sql\Sql;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Adapter\Driver\ResultInterface;
 
 
 class MagentoTable {
@@ -21,11 +24,105 @@ class MagentoTable {
         $this->adapter = $adapter;
     }
 
+    public function dbConnect()
+    {
+        $sql = new Sql($this->adapter);
+        $select = $sql->select('product');
+        $select->columns(array(
+                'id'    =>  'entity_id',
+                'sku'   =>  'productid',
+                'state' =>  'dataState',
+                'lastModified'  =>  'modifieddate',
+            ));
+        $statement = $sql->prepareStatementForSqlObject($select);
+        $result = $statement->execute();
+        $resultSet = new ResultSet;
+        if ($result instanceof ResultInterface && $result->isQueryResult()) {
+            $resultSet->initialize($result);
+        }
+//        This is my query.
+//        var_dump($resultSet);
+//        die();
+        return $resultSet;
+    }
+
+    public function grabSkus()
+    {
+        $data = array();
+        $dirtyCount = 0;
+        $cleanCount = 0;
+        $newCount = 0;
+        $result = $this->dbConnect();
+        $data = $result->toArray();
+        foreach($data as $key => $value){
+//            echo 'haha';
+//            var_dump($value);
+//            echo 'hoho';
+
+            if ('1' == $value['state']){
+                $dirtyCount++;
+            }
+            if ('0' == $value['state']){
+                $cleanCount++;
+            }
+            if ('2' == $value['state']){
+                $newCount++;
+            }
+        }
+//        die();
+//        echo "<pre>";
+//        var_dump($result->toArray());
+//        die();
+
+//        $result->rewind();
+//        $skuID = array();
+//        $sku = array();
+//        $skuLastModifiedDate = array();
+//        while($row = $result->current()){
+//            $result->next();
+//            echo $row['id'] . ' ' . $row['sku'] . ' ' . $row['state'] . "\n";
+//            if($row['state'] == 1){
+//                $dirtyCount++;
+//                $skuLastModifiedDate['dirty'][] = $row['lastModified'];
+//                $skuID['dirty'][] = $row['id'];
+//                $sku['dirty'][] = $row['sku'];
+//            }
+//            if($row['state'] == 0){
+//                $cleanCount++;
+//                $skuLastModifiedDate['clean'][] = $row['lastModified'];
+//                $skuID['clean'][] = $row['id'];
+//                $sku['clean'][] = $row['sku'];
+//            }
+//            if($row['state'] == 2){
+//                $newCount++;
+//                $skuLastModifiedDate['new'][] = $row['lastModified'];
+//                $skuID['new'][] = $row['id'];
+//                $sku['new'][] = $row['sku'];
+//            }
+//
+//        }
+        return array(
+            'data' => $data,
+//            'id' => $skuID,
+//            'sku' => $sku,
+//            'lastModifiedDate' => $skuLastModifiedDate,
+            'dirty' =>  $dirtyCount,
+            'clean' =>  $cleanCount,
+            'new'   =>  $newCount
+        );
+
+    }
+
     public function soapContent()
     {
-        $autodiscover = new AutoDiscover();
-        $autodiscover->setClass(__CLASS__)
-                     ->setUri('https://www.focuscamera.com/index.php/api/soap/index/?wsdl')
+
+
+        $soapClient = new SoapClient('https://www.focuscamera.com/index.php/api/soap/index/?wsdl');
+        $session = $soapClient->login('Adellimore', 'krimson1');
+        $result = $soapClient->call($session, 'catalog_product.update',array(
+            'name'
+        ));
+//        var_dump($result);
     }
 
 } 
