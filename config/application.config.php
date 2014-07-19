2bf26cdc7b7e0456c91ebffa70d6110690c5b540
Will Salazar <was@williamsalazar.com>
2014-07-19 10:26:42 -0400

    adding a wrapper class as a service for Sql wrapper class

--- config/application.config.php
+++ config/application.config.php
@@ -15,6 +15,7 @@ return array(
 //        'ZFTool',
         'Search',
         'Users',
+        'Sql',
     ),
     'module_listener_options' => array(
         'config_glob_paths' => array('config/autoload/{,*.}{global,local}.php'),
---
+++ module/Sql/Module.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: willsalazar
+ * Date: 7/12/14
+ * Time: 8:15 AM
+ */
+
+namespace Sql;
+
+
+class Module {
+
+} 
