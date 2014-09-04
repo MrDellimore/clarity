ba0b190bd8ad3c1a3ee5d9332480dc38a0ca8146
Will Salazar <wsalazar@focuscamera.com>
2014-09-04 16:28:15 -0300

    fixed join in logging table to resolve null issue.

--- module/Logging/src/Logging/Model/LoggingTable.php
+++ module/Logging/src/Logging/Model/LoggingTable.php
@@ -9,6 +9,7 @@
 namespace Logging\Model;
 
 use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Select;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\Adapter\Driver\ResultInterface;
 use Zend\Db\Adapter\Adapter;
@@ -135,8 +136,8 @@ class LoggingTable
         $intTable = new Expression('i.entity_id = logger.entity_id and attribute_id = 102');
         $optionTable = new Expression('o.attribute_id = 102 and o.option_id = i.value');
 
-        $select->join(array('i' => 'productattribute_int'), $intTable ,array('attributeId' => 'attribute_id','optionID' => 'value'));
-        $select->join(array('o' => 'productattribute_option'), $optionTable ,array('manufacturer'=>'value'));
+        $select->join(array('i' => 'productattribute_int'), $intTable ,array('attributeId' => 'attribute_id','optionID' => 'value'), Select::JOIN_LEFT);
+        $select->join(array('o' => 'productattribute_option'), $optionTable ,array('manufacturer'=>'value'), Select::JOIN_LEFT);
         $statement = $this->sql->prepareStatementForSqlObject($select);
         $result = $statement->execute();
 
